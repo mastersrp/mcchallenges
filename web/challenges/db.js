2d37e4e08e88bef7ef1a17238f6fe4e92227bdc0
mastersrp <tcg.thegamer@gmail.com>
2013-05-11 19:58:37 +0200

    Update challenge page

---
+++ web/challenges/db.js
@@ -0,0 +1,12 @@
+function ChallengesCtrl( $scope ) {
+	$scope.challenges = [{
+		"name":"A New World",
+		"url":"01.html"
+	},{
+		"name":"A New Tool",
+		"url":"02.html"
+	},{
+		"name":"Nice house, dawg",
+		"url":"03.html"
+	}]
+}
